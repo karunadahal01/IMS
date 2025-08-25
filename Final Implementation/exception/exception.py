7ddfddf961f21eeab61568abc1a41cf4b7b833d4
karunadahal01 <karunadahal502@gmail.com>
2025-08-25 11:54:19 +0545

    created final implementation

---
+++ Final Implementation/exception/exception.py
@@ -0,0 +1,58 @@
+#################################################################################################
+# Custom Exceptions
+class LoginFailedError(Exception):
+    """Raised when login fails due to invalid credentials or unexpected errors."""
+    pass
+
+
+class ProductAlreadyExistsError(Exception):
+    """Raised when trying to create a product that already exists."""
+    pass
+
+
+class WebDriverInitializationError(Exception):
+    """Raised when WebDriver initialization fails."""
+    pass
+
+
+class NavigationError(Exception):
+    """Raised when navigation to a menu or page fails."""
+    pass
+
+
+class FormFieldNotFoundError(Exception):
+    """Raised when a required form field is not found or not interactable."""
+    pass
+
+
+class PopupHandlingError(Exception):
+    """Raised when a popup/modal cannot be handled as expected."""
+    pass
+
+
+class SaveInvoiceError(Exception):
+    """Raised when saving the invoice fails."""
+    pass
+
+
+class ProductCreationError(Exception):
+    """Raised when trying to create a product that already exists."""
+    pass
+
+
+class PurchaseNotSuccessError(Exception):
+    """Raised when the purchase transaction does not complete successfully."""
+    pass
+
+
+class SaveSalesInvoiceError(Exception):
+    """Raised when saving the sales invoice fails."""
+    pass
+
+
+class ListNotFoundError(Exception):
+    """Raised when saving the sales invoice fails."""
+    pass
+
+#################################################################################################
+ 
